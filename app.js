7db027af9c174ecc37ef8713afc02e6b9bb2b3ca
Yash Jain <jainy5855@gmail.com>
2025-02-05 17:52:31 +0530

    Front end using tailwind and flowbite, for user register

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const userRouter = require('./routes/user.routers')
+const userRouter = require('./routes/user.routes.js')
 
 const app = express();
 
 app.set('view engine', 'ejs')
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use('/user', userRouter)
 
