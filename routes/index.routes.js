42cdfec7eec68776a7d2a961214da9481206c6db
Yash Jain <jainy5855@gmail.com>
2025-02-10 15:03:53 +0530

    User Authorization

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,16 +2,19 @@ const express = require('express')
 const router = express.Router();
 const upload = require('../config/multer.config.js')
 const fileModel = require('../models/files.model.js')
+const authMiddleware = require('../middlewares/auth.js');
 
-router.get('/home', (req, res) => {
+router.get('/home', authMiddleware, (req, res) => {
     res.render('home');
 })
 
-router.post('/upload', upload.single('file'), async (req, res) => {
+router.post('/upload', authMiddleware , upload.single('file'), async (req, res) => {
     const newFile = await fileModel.create({
         path: req.file.path,
         originalname: req.file.originalname,
+        user: req.user.userId
     })
+    res.json(newFile)
 })
 
 module.exports = router;
