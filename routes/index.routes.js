6eb1d3600c23b9685ea3c10714e1cd499d04e863
Yash Jain <jainy5855@gmail.com>
2025-02-10 15:21:51 +0530

    Accessing all files uploaded by a user and downloading from frontend

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,8 +4,14 @@ const upload = require('../config/multer.config.js')
 const fileModel = require('../models/files.model.js')
 const authMiddleware = require('../middlewares/auth.js');
 
-router.get('/home', authMiddleware, (req, res) => {
-    res.render('home');
+router.get('/home', authMiddleware, async (req, res) => {
+    const userFiles = await fileModel.find({
+        user: req.user.userId
+    })
+    console.log(userFiles)
+    res.render('home', {
+        files: userFiles
+    });
 })
 
 router.post('/upload', authMiddleware , upload.single('file'), async (req, res) => {
@@ -15,6 +21,21 @@ router.post('/upload', authMiddleware , upload.single('file'), async (req, res) 
         user: req.user.userId
     })
     res.json(newFile)
+})
+
+router.get('/download/:path', authMiddleware, async (req, res) => {
+    const loggedInUserId = req.user.userId;
+    const path = req.params.path;
+    const file = await fileModel.findOne({
+        user: loggedInUserId,
+        path: path
+    })
+
+    if(!file){
+        return res.status(401).json({
+            message: 'Unauthorized'
+        })
+    }
 })
 
 module.exports = router;
