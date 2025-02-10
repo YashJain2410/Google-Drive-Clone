1c38e6b708cc0b18c4593f687ae71364bade8c80
Yash Jain <jainy5855@gmail.com>
2025-02-10 14:52:30 +0530

    File model

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const router = express.Router();
 const upload = require('../config/multer.config.js')
+const fileModel = require('../models/files.model.js')
 
 router.get('/home', (req, res) => {
     res.render('home');
 })
 
-router.post('/upload', upload.single('file'), (req, res) => {
-    res.send(req.file)
+router.post('/upload', upload.single('file'), async (req, res) => {
+    const newFile = await fileModel.create({
+        path: req.file.path,
+        originalname: req.file.originalname,
+    })
 })
 
 module.exports = router;
